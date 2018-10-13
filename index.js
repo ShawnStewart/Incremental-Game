963aa051ccbf784de01a6ad471229ab64220a54c
Shawn Stewart <s.stewart0220@gmail.com>
2018-10-12 21:21:06 -0400

    Saving progress to local storage. Updated progress bar styling. Tracking game start time and lifetime widgets

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ let gameData = {
   widgetCount: 0
 };
 
+// Check for saved game
+if (localStorage.gameData) {
+  gameData = JSON.parse(localStorage.gameData);
+}
+
 const widgetCount = document.getElementById("widgetCount");
+const widgetTotal = document.getElementById("widgetTotal");
+
 setInterval(() => {
+  localStorage.setItem("gameData", JSON.stringify(gameData));
   widgetCount.innerHTML = gameData.widgetCount;
-}, 1);
+  widgetTotal.innerHTML = gameData.widgetTotal;
+}, 100);
 
 widgetButton = () => {
-  const progressBar = document.getElementById("widgetProgress__Bar");
+  // Set start date if first click.
+  if (!gameData.startTime) {
+    gameData.startTime = Date.now();
+  }
+
+  const progressBar = document.getElementById("widgetProgress");
   const btn = document.getElementById("widgetButton");
   btn.disabled = true;
   let progress = 0;
@@ -19,6 +33,7 @@ widgetButton = () => {
       clearInterval(widgetInterval);
       btn.disabled = false;
       gameData.widgetCount++;
+      gameData.widgetTotal++;
       progressBar.style.width = "0%";
     } else {
       progress++;
